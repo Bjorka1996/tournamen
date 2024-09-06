2f29b9c6cfb2f1e8539adac8d9328c0972b1f3b7
Bjorka1996 <ridhozulfikar966@gmail.com>
2024-09-07 05:10:10 +0700

    selesai semua sisa responsive yang belum.. semua tutor sudah selesai di youtube

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-let menuicon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-let sections = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('header nava');
-
-
+let menuicon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-  sections.forEach(sec => {
+  sections.forEach((sec) => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight
-    let id = sec.getAttribute('id');
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-    if(top >)
-  })
-}
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach((links) => {
+        links.classList.remove("active");
+        document.querySelector("header nav a[href*='" + id + "']").classList.add("active");
+      });
+    }
+  });
+};
 
 menuicon.onclick = () => {
-  menuicon.classList.toggle('bx-x');
-  navbar.classList.toggle('active');
-}
+  menuicon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
